8b8bdfa0ad0b2c404db6eea4c0f46e89a685ef4c
K.C. Hara <kondwani@baobabhealth.org>
2009-05-05 12:03:33 +0200

    ignored logs, modified app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,14 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  before_filter :verify_user
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
+  filter_parameter_logging :password
+  def verify_user
+    @user=User.find(session[:user_id]) if session[:user_id]
+    @user_logged_in=session[:user_id]? true : false
+  end
 end
